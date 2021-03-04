de6e14548c7f15895207b31781b9b0d6ffc97dc9
Tiaguin061 <tiaguin180@gmail.com>
2021-03-04 20:12:20 -0300

    feat: Creating authentication service and ensure

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: "25a9c640683d8f4f351dcd4f34f9770d",
+    expiresIn: '10d',
+  }
+}
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+import AppError from '../errors/AppError';
+
+import authConfig from '../config/auth';
+import { verify } from 'jsonwebtoken';
+
+interface TokenPayload {
+  exp: number;
+  iat: number
+  sub: string;
+}
+
+export default function ensureAuthenticated(request: Request, response: Response, next: NextFunction): void {
+  const authHeader = request.headers.authorization;
+
+  if(!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    }
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
