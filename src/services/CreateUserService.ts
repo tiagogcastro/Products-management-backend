55d7ae6759ea0829e8e7ee782e991a8f16ef1b2d
Tiaguin061 <tiaguin180@gmail.com>
2021-03-04 17:37:14 -0300

    feat: Creating User service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import { hash } from 'bcryptjs';
+
+import { User } from '../models/User';
+import {UsersRepository} from '../repositories/UsersRepository';
+
+interface Request{
+  email: string;
+  name: string;
+  password: string;
+}
+
+class CreateUserService {
+  async execute({email, name, password}: Request): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const userExist = await usersRepository.findOne({
+      where: {email}
+    });
+
+    if(userExist) {
+      throw new AppError('User already exist.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      email,
+      name,
+      password: hashedPassword
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
