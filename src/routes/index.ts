0a326fee10fedef39aa9c8bc36da2b516c812ba5
Tiaguin061 <tiaguin180@gmail.com>
2021-03-04 20:12:44 -0300

    feat: Creating session route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router} from 'express';
 import usersRouter from './users.routers';
+import sessionsRouter from './session.routers';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
