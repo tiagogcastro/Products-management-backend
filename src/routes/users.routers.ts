01a5ace0aadbd5bce156facaf3d07ca582198048
Tiaguin061 <tiaguin180@gmail.com>
2021-03-11 17:23:23 -0300

    feat: Adding errors, generating token when registering

--- src/routes/users.routers.ts
+++ src/routes/users.routers.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { sign } from 'jsonwebtoken';
+import { getRepository } from 'typeorm';
+import auth from '../config/auth';
+import AppError from '../errors/AppError';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import { User } from '../models/User';
 import CreateUserService from '../services/CreateUserService';
+import ChangePasswordService from '../services/Login/ChangePasswordService';
+import UpdateUserService from '../services/UpdateUserService';
 
 const usersRouter = Router();
 
@@ -14,13 +22,29 @@ usersRouter.post('/create', async (request, response) => {
     password,
   });
 
-  return response.json(user);
+  const { secret, expiresIn } = auth.jwt;
+
+  const token = sign({
+
+  }, secret, {
+    subject: user.id,
+    expiresIn,
+  });
+
+  return response.json({
+    user, 
+    token
+  });
 });
 
 usersRouter.get('/', ensureAuthenticated, async (request, response) => {
   const userRepository = getRepository(User);
 
   const user = await userRepository.findOne(request.user.id);
+
+  if(!user) {
+    throw new AppError('Você não está logado!');
+  }
 
   return response.json({ user });
 });
@@ -43,11 +67,20 @@ usersRouter.put('/edit', ensureAuthenticated, async (request, response) => {
 });
 
 usersRouter.delete('/delete', ensureAuthenticated, async (request, response) => {
+  const id = request.user.id
   const userRepository = getRepository(User);
 
-  await userRepository.delete(request.user.id);
+  const user = await userRepository.findOne(id);
+  
+  if(!user) {
+    throw new AppError('Este usuário não existe');
+  }
+  
+  await userRepository.delete(id);
 
-  return response.send();
+  return response.json({
+    success: "Usuário deletado com sucesso."
+  });
 });
 
 // Sem está logado
@@ -56,7 +89,7 @@ usersRouter.delete('/delete', ensureAuthenticated, async (request, response) => 
 // });
 
 // Logado
-usersRouter.put('/password', ensureAuthenticated, async (request, response) => {
+usersRouter.put('/alter-password', ensureAuthenticated, async (request, response) => {
   const id = request.user.id;
 
   const { password, newPassword, repeatPassword } = request.body;
